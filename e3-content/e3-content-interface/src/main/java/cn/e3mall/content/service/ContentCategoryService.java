bbb64977b1ce0f7a688190d7c3809868f1c0e7a5
AlexYansen <30942303+AlexYansen@users.noreply.github.com>
2018-03-29 10:28:50 +0800

    Add files via upload

---
+++ e3-content/e3-content-interface/src/main/java/cn/e3mall/content/service/ContentCategoryService.java
@@ -0,0 +1,12 @@
+package cn.e3mall.content.service;
+
+import java.util.List;
+
+import cn.e3mall.common.pojo.EasyUITreeNode;
+import cn.e3mall.common.utils.E3Result;
+
+public interface ContentCategoryService {
+
+	List<EasyUITreeNode> getContentCatList(long parentId);
+	E3Result addContentCategory(long parentId, String name);
+}
