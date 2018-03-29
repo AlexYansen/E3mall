961bedd24913caac783b4e6a3c66aaa447623eda
AlexYansen <30942303+AlexYansen@users.noreply.github.com>
2018-03-29 09:00:10 +0800

    Add files via upload

---
+++ e3-manager/e3-manager-interface/src/main/java/cn/e3mall/service/ItemService.java
@@ -0,0 +1,14 @@
+package cn.e3mall.service;
+
+import cn.e3mall.common.pojo.EasyUIDataGridResult;
+import cn.e3mall.common.utils.E3Result;
+import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemDesc;
+
+public interface ItemService {
+
+	TbItem getItemById(long itemId);
+	EasyUIDataGridResult getItemList(int page, int rows);
+	E3Result addItem(TbItem item, String desc);
+	TbItemDesc getItemDescById(long itemId);
+}
