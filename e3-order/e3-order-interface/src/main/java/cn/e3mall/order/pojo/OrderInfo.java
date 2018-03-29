b679f6539cd981a6fa7cb00117fcf54ac78d2179
AlexYansen <30942303+AlexYansen@users.noreply.github.com>
2018-03-29 11:17:10 +0800

    Add files via upload

---
+++ e3-order/e3-order-interface/src/main/java/cn/e3mall/order/pojo/OrderInfo.java
@@ -0,0 +1,27 @@
+package cn.e3mall.order.pojo;
+
+import java.io.Serializable;
+import java.util.List;
+
+import cn.e3mall.pojo.TbOrder;
+import cn.e3mall.pojo.TbOrderItem;
+import cn.e3mall.pojo.TbOrderShipping;
+
+public class OrderInfo extends TbOrder implements Serializable {
+
+	private List<TbOrderItem> orderItems;
+	private TbOrderShipping orderShipping;
+	public List<TbOrderItem> getOrderItems() {
+		return orderItems;
+	}
+	public void setOrderItems(List<TbOrderItem> orderItems) {
+		this.orderItems = orderItems;
+	}
+	public TbOrderShipping getOrderShipping() {
+		return orderShipping;
+	}
+	public void setOrderShipping(TbOrderShipping orderShipping) {
+		this.orderShipping = orderShipping;
+	}
+	
+}
